5250b735d4aa4e95737c974b2417eda5d7e5646b
xm22135 <xm22135@my.westlake.school.nz>
2024-10-16 09:47:49 +1300

    home done, season history next

--- app.py
+++ app.py
@@ -3,15 +3,22 @@ import sqlite3
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('chelsea.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db_connection()
+    with app.open_resource('schema.sql') as f:
+        conn.executescript(f.read().decode('utf8'))
+    conn.close()
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-
-
-
-
-
+@app.route('/seasonhistory', methods = ('POST','GET'))
 
 if __name__ == '__main__':
     app.run(debug = True)
