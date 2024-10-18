043f5daf276d8f37af0eb1958a40081f0df1b50e
xm22135 <xm22135@my.westlake.school.nz>
2024-10-18 14:19:48 +1300

    add almost done

--- app.py
+++ app.py
@@ -26,5 +26,26 @@ def seasonhistory():
     conn.close
     return render_template('seasonhistory.html', seasons = seasons)
     
+@app.route('/addrecords', methods = ('POST','GET'))
+def addrecords():
+    if request.method == 'POST':
+        season = request.form['season']
+        games_played = request.form['games_played']
+        games_won = request.form['games_won']
+        games_drawn = request.form['games_drawn']
+        games_lost = request.form['games_lost']
+        goals_for = request.form['goals_for']
+        goals_against = request.form['goals_against']
+        goal_difference = request.form['goal_difference']
+        points = request.form['points']
+        top_goal_scorer = request.form['top_goal_scorer']
+        top_assist_maker = request.form['top_assist_maker']
+        conn = get_db_connection()
+        conn.execute('INSERT INTO seasons (season,games_played,games_won,games_drawn,games_lost,goals_for,goals_against,goal_difference,points,top_goal_scorer,top_assist_maker) Values (?,?,?,?,?,?,?,?,?,?,?)', (season,games_played,games_won,games_drawn,games_lost,goals_for,goals_against,goal_difference,points,top_goal_scorer,top_assist_maker))
+        conn.commit()
+        conn.close
+        return render_template('addrecords.html')
+    return render_template('addrecords.html')
+
 if __name__ == '__main__':
     app.run(debug = True)
